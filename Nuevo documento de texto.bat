bcfa186425590515d8099069c031f4b246df7b55
unknown <jannoguer@inspladelestany.cat>
2022-11-24 19:17:40 +0100

    hola

---
+++ Nuevo documento de texto.bat
@@ -0,0 +1,7 @@
+echo "# xAbiVestDeb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Manel000012/xAbiVestDeb.git
+git push -u origin main
