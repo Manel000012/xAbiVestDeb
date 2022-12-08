77409a428661df9d1108dfa1ea2474bf525f9f02
Manel000012 <119004942+Manel000012@users.noreply.github.com>
2022-12-08 11:33:23 +0100

    Versio 1.2
    Modificacions:
    
    -Tot el contingut HTML dels Jugadors Fet
    
    -Tot el contingut css dels Jugadors Fet
    
    -Tot el contingut JS dels Jugadors Fet
    
    -Informació dels jugadors afegida
    
    -Escuts afegits

--- index.js
+++ index.js
@@ -1,3 +1,94 @@
-var popover = new bootstrap.Popover(document.querySelector('.popover'), {
+var popover = new bootstrap.Popover(document.querySelector('.terestegen'), {
   container: 'body'
 })
+
+var popover = new bootstrap.Popover(document.querySelector('.araujo'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.Kounde'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.Balde'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.JordiAlba'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.Pedri'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.Gavi'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.dejong'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.lewandowski'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.rafiña'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.usman'), {
+  container: 'body'
+})
+//Madrid//
+var popover = new bootstrap.Popover(document.querySelector('.curtua'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.militao'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.rudiger'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.mendy'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.carvajal'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.tchouameni'), {
+  container: 'body'
+})
+
+
+var popover = new bootstrap.Popover(document.querySelector('.fede'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.modric'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.rodrygo'), {
+  container: 'body'
+})
+
+var popover = new bootstrap.Popover(document.querySelector('.benzema'), {
+  container: 'body'
+
+})
+var popover = new bootstrap.Popover(document.querySelector('.vini'), {
+  container: 'body'
+})
+
+
+
+
+
+  
